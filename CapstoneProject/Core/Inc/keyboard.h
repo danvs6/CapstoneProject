cf708a4f09915581556e460fb80f844f1eb14d44
danvs6 <wgj6fc@virginia.edu>
2024-10-01 14:45:14 -0400

    Created keyboard header and source files

---
+++ CapstoneProject/Core/Inc/keyboard.h
@@ -0,0 +1,13 @@
+/*
+ * keyboard.h
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: maste
+ */
+
+#ifndef INC_KEYBOARD_H_
+#define INC_KEYBOARD_H_
+
+
+
+#endif /* INC_KEYBOARD_H_ */
---
+++ CapstoneProject/Core/Src/keyboard.c
@@ -0,0 +1,11 @@
+/*
+ * keyboard.c
+ *
+ *  Created on: Oct 1, 2024
+ *      Author: maste
+ */
+
+#include "keyboard.h"
+
+
+
