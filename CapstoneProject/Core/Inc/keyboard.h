7a9412f698c0c4e241abed3041b508b63352e4cf
danvs6 <wgj6fc@virginia.edu>
2024-10-01 20:13:27 -0400

    Added function declarations for the matrix keyboard

--- CapstoneProject/Core/Inc/keyboard.h
+++ CapstoneProject/Core/Inc/keyboard.h
@@ -1,13 +1,13 @@
-/*
- * keyboard.h
- *
- *  Created on: Oct 1, 2024
- *      Author: maste
- */
-
 #ifndef INC_KEYBOARD_H_
 #define INC_KEYBOARD_H_
 
+// Set the control pins based on the channel (binary)
+void setMuxChannel(uint8_t channel);
 
+// Read the state of the COM pin (connected to the MUX)
+uint8_t readMuxInput(void);
+
+// Scan the matrix keyboard continuously
+void scanKeyboardMatrix(void);
 
 #endif /* INC_KEYBOARD_H_ */
